affed7a7870e7a957ba03aa8655937caf9a6348f
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-02-27 01:47:12 -0300

    PersonService and PersonController constructors arguments reduced to annotation

--- src/main/java/dev/danielmarcl/personapi/service/PersonService.java
+++ src/main/java/dev/danielmarcl/personapi/service/PersonService.java
@@ -5,23 +5,19 @@ import dev.danielmarcl.personapi.exceptions.PersonNotFoundException;
 import dev.danielmarcl.personapi.mappers.PersonMapper;
 import dev.danielmarcl.personapi.model.Person;
 import dev.danielmarcl.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__({@Autowired})) // create an all-args constructor with the anotation @Autowired
 public class PersonService {
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired // injetar um objeto do tipo especificado
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public List<PersonDTO> getPersons() {
         List<Person> listPerson = this.personRepository.findAll();
